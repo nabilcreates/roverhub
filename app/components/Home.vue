ebe2b155164a6bb996a19479e2c893d1af563b65
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-03 17:46:47 +0800

    ** SOL UPDATE ** Search for pictures by SOL

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,16 +1,17 @@
 <template>
 
-        <ScrollView>
-            <StackLayout>
-                <Label textWrap="true" class="h1" style="color: white; font-weight: 700;">{{app.name}}</Label>
-                <Label textWrap="true" class="h3" style="color: white; font-weight: 700;">This app gathers the latest pictures taken by NASA's Curiosity Rover</Label>
-                <Label textWrap="true">The images may load slowly (this depends on your internet connection). I am not responsible for your data usage or anything that happens to your phone</Label>
+    <ScrollView>
+        <StackLayout>
+            <Label textWrap="true" class="h1" style="color: white; font-weight: 700;">{{app.name}}</Label>
+            <Label textWrap="true" class="h3" style="color: white; font-weight: 700;">This app gathers the latest pictures taken by NASA's Curiosity Rover</Label>
+            <Label textWrap="true">The images may load slowly (this depends on your internet connection). I am not responsible for your data usage or anything that happens to your phone</Label>
 
+            <Label textWrap="true">Sol means the number of days since the Curiosity is on Mars</Label>
 
-                <Label textWrap="true" class="h2" style="color: white; font-weight: 700;">Swipe To Continue ➡️</Label>
-                
-            </StackLayout>
-        </ScrollView>
+            <Label textWrap="true" class="h2" style="color: white; font-weight: 700;">Swipe To Continue ➡️</Label>
+
+        </StackLayout>
+    </ScrollView>
 
 </template>
 
@@ -26,10 +27,11 @@
                     name: "",
                     version: "",
                 },
+
             }
         },
 
-        mounted(){
+        mounted() {
             this.app.name = appconfig.name;
         }
 
--- app/components/Pictures.vue
+++ app/components/Pictures.vue
@@ -1,21 +1,35 @@
 <template>
         <StackLayout>
 
-            <ListView v-if="loadedAPI" class="list-group" for="data in apiData.photos" @itemTap="onItemTap" style="height:1250px">
-                <v-template>
-                    <StackLayout flexDirection="row" class="list-group-item">
-                        <!-- DISPLAY THE IMAGE -->
-                        <Image :src="data.img_src" />
+            <Label textWrap='true'>Max Martian Sol: {{max_sol}}</Label>
 
-                        <!-- DISPLAY DATE OF PHOTO TAKEN -->
-                        <Label textWrap='true'>📅: {{data.earth_date}}</Label>
-                        <Label textWrap='true'>📷: {{data.camera.full_name}} ({{data.camera.name}})</Label>
-                        
-                    </StackLayout>
-                </v-template>
-            </ListView>
+            <TextField v-model="value" />
+            <Slider value="max_sol" v-model="value" minValue='0' :maxValue='max_sol' />
+            <Button @tap="getApiData(value)">Get Pictures</Button>
 
-            <Label v-else>Loading Pictures... This may take around 10-60 seconds</Label>
+            <StackLayout v-if="loadedAPI">
+
+                <!-- DISPLAY THIS IF APIDATA.PHOTOS.LENGTH = 0 OR IN OTHER WORDS, NO PICS ON THAT SOL -->
+                <Label textWrap='true' v-if="apiData.photos.length == 0">No Pics :(</Label>
+
+                <ListView class="list-group" for="data in apiData.photos" @itemTap="onItemTap" style="height:1250px">
+                    <v-template>
+                        <StackLayout flexDirection="row" class="list-group-item">
+                            <!-- DISPLAY THE IMAGE -->
+                            <Image :src="data.img_src" />
+
+                            <!-- DISPLAY DATE OF PHOTO TAKEN -->
+                            <Label textWrap='true'>📅: {{data.earth_date}}</Label>
+                            <Label textWrap='true'>📷: {{data.camera.full_name}} ({{data.camera.name}})</Label>
+
+                        </StackLayout>
+                    </v-template>
+                </ListView>
+
+            </StackLayout>
+
+
+
         </StackLayout>
 </template>
 
@@ -32,18 +46,23 @@
                     version: "",
                 },
 
+                value: 2218,
+                max_sol: "",
+
                 api_key: "0Ls3Ro5l2tWOuHLvK3N8roehpIZxpYxIuK0WN9AZ",
                 apiData: [],
                 apiDateData: "",
 
                 loadedAPI: false,
                 loadedDate: false,
+
+                sol: ""
             }
         },
 
         methods: {
 
-            getLatestDate() {
+            getLatestSol() {
 
                 this.loadedDate = false;
 
@@ -55,32 +74,34 @@
 
                         if (this.loadedDate) {
                             this.apiDateData = json
-                            console.log(this.apiDateData.photos[0].rover.max_date)
 
-                            // RUN GETAPIDATA
-                            this.getApiData()
+                            // SOL = json.photos[0].rover.max_sol
+                            // SET VALUE TO LATEST SOL
+                            this.max_sol = json.photos[0].rover.max_sol
                         }
                     })
 
             },
 
-            getApiData() {
+            getApiData(solnum) {
+
+                console.log(solnum)
+                console.log("getting api")
+
                 this.loadedAPI = false;
-                fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + this.apiDateData
-                        .photos[0].rover.max_date +
-                        "&api_key=" + this.api_key)
+                fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" + solnum + "&api_key=" +
+                        this.api_key)
                     .then(res => res.json())
                     .then(json => {
-
                         this.apiData = json;
                         this.loadedAPI = true;
+                        console.log("done")
 
-                        console.log(json)
                     })
 
             },
 
-            onItemTap(args){
+            onItemTap(args) {
                 var index = args.index
                 console.log(index)
                 utilsModule.openUrl(this.apiData.photos[args.index].img_src)
@@ -89,11 +110,7 @@
         },
 
         mounted() {
-            // this.app.name = appconfig.name;
-            // this.app.version = appconfig.version;
-
-            this.getLatestDate()
-
+            this.getLatestSol();
         }
 
     }
@@ -108,15 +125,15 @@
         color: #ffffff;
     }
 
-    StackLayout{
+    StackLayout {
         margin: 0 10;
     }
-    
-    Label{
+
+    Label {
         color: white;
     }
-    
-    Page{
+
+    Page {
         background-color: #161616;
         color: white;
     }
@@ -128,5 +145,4 @@
         border-radius: 1000;
         height: 38;
     }
-    
 </style>
